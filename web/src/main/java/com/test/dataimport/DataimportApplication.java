cc7b92d61a59260dd4a57256a07dc113c2a4d529
leeconnor <leeconnor@126.com>
2018-07-01 22:19:05 +0800

    提交

--- web/src/main/java/com/test/dataimport/DataimportApplication.java
+++ web/src/main/java/com/test/dataimport/DataimportApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class DataimportApplication {
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(DataimportApplication.class, args);
 	}
 }
